bca7be79a6cee73164f78fd59d7cc5715972ba8a
mwilliams <mwilliams@azavea.com>
2019-09-23 16:24:48 -0400

    testing build for lambdas

--- lambda/svgo.js
+++ lambda/svgo.js
@@ -1,19 +1,135 @@
-import SVGO from "svgo";
+'use strict';
 import querystring from "querystring";
 
 exports.handler = async (event, context) => {
-  // Only allow POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
+  var SVGO = require('svgo'),
+  svgo = new SVGO({
+    plugins: [
+      {
+        cleanupAttrs: true
+      },
+      {
+        removeDoctype: true
+      },
+      {
+        removeXMLProcInst: true
+      },
+      {
+        removeComments: true
+      },
+      {
+        removeMetadata: true
+      },
+      {
+        removeTitle: true
+      },
+      {
+        removeDesc: true
+      },
+      {
+        removeUselessDefs: true
+      },
+      {
+        removeEditorsNSData: true
+      },
+      {
+        removeEmptyAttrs: true
+      },
+      {
+        removeHiddenElems: true
+      },
+      {
+        removeEmptyText: true
+      },
+      {
+        removeEmptyContainers: true
+      },
+      {
+        removeViewBox: false
+      },
+      {
+        cleanupEnableBackground: true
+      },
+      {
+        convertStyleToAttrs: true
+      },
+      {
+        convertColors: true
+      },
+      {
+        convertPathData: true
+      },
+      {
+        convertTransform: true
+      },
+      {
+        removeUnknownsAndDefaults: true
+      },
+      {
+        removeNonInheritableGroupAttrs: true
+      },
+      {
+        removeUselessStrokeAndFill: true
+      },
+      {
+        removeUnusedNS: true
+      },
+      {
+        cleanupIDs: true
+      },
+      {
+        cleanupNumericValues: true
+      },
+      {
+        moveElemsAttrsToGroup: true
+      },
+      {
+        moveGroupAttrsToElems: true
+      },
+      {
+        collapseGroups: true
+      },
+      {
+        removeRasterImages: false
+      },
+      {
+        mergePaths: true
+      },
+      {
+        convertShapeToPath: true
+      },
+      {
+        sortAttrs: true
+      },
+      {
+        removeDimensions: true
+      },
+      {
+        removeAttrs: { attrs: "(stroke|fill)" }
+      }
+    ]
+    });
+
+  try {
+    const svgData = `
+      <svg>
+        <line
+          stroke= "#bada55"
+          stroke-width= "2"
+          stroke-linecap= "round"
+          x1= "70"
+          y1= "80"
+          x2= "250"
+          y2= "150">
+        </line>
+      </svg>
+    `;
+    const optimizedObject = await svgo.optimize(svgData);
+    return {
+      statusCode: 200,
+      body: optimizedObject.data
+    }
+  } catch (e) {
+    throw e;
   }
-
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body);
-  const name = params.name || "World";
-
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
 };
